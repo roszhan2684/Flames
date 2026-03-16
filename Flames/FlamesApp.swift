b4282192a1ddfc45a64414b671f9d131f65fed4e
roszhan raj <68181452+ROSZHAN@users.noreply.github.com>
2026-03-16 11:40:22 -0700

    Initial Commit

---
+++ Flames/FlamesApp.swift
@@ -0,0 +1,32 @@
+//
+//  FlamesApp.swift
+//  Flames
+//
+//  Created by ROSZHAN RAJ on 16/03/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FlamesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
